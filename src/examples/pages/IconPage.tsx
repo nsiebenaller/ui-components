d70b40bd9dd014e6e31c6f2fcd706d75d2d12ce1
nicholassiebenaller <nicholaspaulsiebenaller@gmail.com>
2020-06-18 14:38:42 -0400

    format project for github pages

--- src/examples/pages/IconPage.tsx
+++ src/examples/pages/IconPage.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { Icon } from "../../package/dist";
-import { IconRow } from './style'
 
 export default function IconPage() {
-  return (
-    <div>
-      <h1>Icon Component</h1>
-      <IconWithLabel iconName={"ArrowDropDown"} />
-      <IconWithLabel iconName={"CheckBox"} />
-      <IconWithLabel iconName={"CheckBoxOutlineBlank"} />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Icon Component</h1>
+            <IconWithLabel iconName={"ArrowDropDown"} />
+            <IconWithLabel iconName={"CheckBox"} />
+            <IconWithLabel iconName={"CheckBoxOutlineBlank"} />
+            <IconWithLabel iconName={"CalendarToday"} />
+        </div>
+    );
 }
 
 interface Props {
     iconName: string;
 }
 const IconWithLabel = (props: Props) => (
-    <IconRow>
-        <Icon iconName={props.iconName} />
-        <span>{props.iconName}</span>
-    </IconRow>
-)
+    <div>
+        <hr />
+        <div>
+            <Icon iconName={props.iconName} />
+            <span>{props.iconName}</span>
+        </div>
+    </div>
+);
--- src/examples/pages/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const IconRow = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 10px 0px;
-    border-top: 1px solid lightgrey;
-`
