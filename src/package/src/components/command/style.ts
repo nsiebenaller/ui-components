0cbcd02708b5078d8ae7d2fc540bd9d0f2178701
nicholassiebenaller <nicholaspaulsiebenaller@gmail.com>
2020-08-06 14:26:01 -0400

    build 1.0.38 - modal fixes

--- src/package/src/components/command/style.ts
+++ src/package/src/components/command/style.ts
@@ -10,6 +10,8 @@ export const Actions = styled.div`
 
 export const HookStyle = `
     position: fixed;
+    top: 0;
+    left: 0;
     height: 100%;
     width: 100%;
     background-color: rgba(0, 0, 0, 0.5);
